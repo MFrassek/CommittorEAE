227fbb908ab8491bfa4aa47b776884321df74e11
Martin <martinfrassek@gmx.de>
2020-06-10 10:41:03 +0200

    Extended trimmer.py by static method to trim pB_dict

--- NucleationModel/trimmer.py
+++ NucleationModel/trimmer.py
@@ -1,5 +1,3 @@
-
-
 class Trimmer():
 	def __init__(self, pBs):
 		self._pBs = pBs
@@ -7,3 +5,8 @@ class Trimmer():
 
 	def trim(self, list_to_trim):
 		return list_to_trim[self._keep_elements]
+
+	@staticmethod
+	def trim_dict(dict_to_trim):
+		return {key: label for key, label in dict_to_trim.items() \
+			if label != 0 and label != 1}
