5aadf99c90a6785d6e13db7b06c0a6915af84fbf
Martin <martinfrassek@gmx.de>
2020-06-10 11:41:46 +0200

    Changed pB_balancer variable names from normed to balanced.

--- NucleationModel/pB_balancer.py
+++ NucleationModel/pB_balancer.py
@@ -7,17 +7,18 @@ class pB_Balancer():
 		pBs_len = len(pBs)
 		round_pBs = np.ceil((np.array(pBs) * (bins + 1)))
 		counter = Counter(round_pBs)
-		normed_counter = {key: pBs_len/label for key, label in counter.items()}
-		pB_normed_weights = np.array([normed_counter[i] for i in round_pBs])
-		return pB_normed_weights
+		balanced_counter = {key: pBs_len/label for key, label \
+			in counter.items()}
+		pB_balanced_weights = np.array([balanced_counter[i] for i in round_pBs])
+		return pB_balanced_weights
 
 	@staticmethod
 	def balance_and_trim(pBs, bins):
 		pBs_len = len(pBs)
 		round_pBs = np.ceil((np.array(pBs) * (bins + 1)))
 		counter = Counter(round_pBs)
-		trimmed_normed_counter = {key: pBs_len/label if key != 0 and \
+		trimmed_balanced_counter = {key: pBs_len/label if key != 0 and \
 			key != bins + 1 else 0 for key, label in counter.items()}
-		trimmed_pB_normed_weights = np.array([trimmed_normed_counter[i] \
+		trimmed_pB_balanced_weights = np.array([trimmed_balanced_counter[i] \
 			for i in round_pBs])
-		return trimmed_pB_normed_weights
+		return trimmed_pB_balanced_weights
