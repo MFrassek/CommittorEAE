9267268c06a1bc4a16355f26ba2352a5ca4292a3
Martin <martinfrassek@gmx.de>
2020-06-16 10:31:09 +0200

    Add new loss functions and name functions consistently
    For testing purposes added loss functions that determine the absolute or
    squared error of a prediction, as well as the loss thereof.
    Removed dashes from function names and went for camelCase.

--- NucleationModel/losses.py
+++ NucleationModel/losses.py
@@ -1,24 +1,36 @@
 import tensorflow as tf
 
-def binary_neg_likelihood(y_actual, y_pred):
+def binaryNegLikelihood(y_actual, y_pred):
 	# nB log pB + nA log (1-pB)
 	return -(y_actual * tf.math.log(y_pred) \
 		+ (1-y_actual) * tf.math.log(1-y_pred))
 
-def binomial_neg_likelihood(y_actual, y_pred):
+def binomialNegLikelihood(y_actual, y_pred):
 	# label here symbolizes number of paths that ended in B (0,1,2)
 	return -(2*y_actual * tf.math.log(y_pred) \
 		+ (2*(1-y_actual)) * tf.math.log(1-y_pred))
 
-def log_loss(y_actual, y_pred):
+def logLoss(y_actual, y_pred):
 	return tf.math.log(abs(y_actual-y_pred))
 
-def difference_of_logs(y_actual, y_pred):
+def differenceOfLogs(y_actual, y_pred):
 	#return tf.math.log(y_actual)
 	return abs(tf.math.log(y_actual)-tf.math.log(y_pred))
 
-def KL_divergence(y_actual, y_pred):
+def KLDivergence(y_actual, y_pred):
 	return y_actual * tf.math.log(abs(y_actual/y_pred))
 
-def KL_divergence_inv(y_actual, y_pred):
+def KLDivergenceInv(y_actual, y_pred):
 	return y_pred * tf.math.log(abs(y_pred/y_actual))
+
+def absoluteError(y_actual, y_pred):
+	return abs(y_actual - y_pred)
+
+def logAbsoluteError(y_actual, y_pred):
+	return tf.math.log(abs(y_actual - y_pred))
+
+def squaredError(y_actual, y_pred):
+	return (y_actual - y_pred)**2
+
+def logSquaredError(y_actual, y_pred):
+	return tf.math.log((y_actual - y_pred)**2)
