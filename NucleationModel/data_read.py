df71b2bca67fbf46809c4e990f2c4d8aadac44f8
Martin <martinfrassek@gmx.de>
2020-06-18 16:36:19 +0200

    Added function to read a file of shooting points as a test case
    Added read_shooting_points(). The function returns the list of shooting
    points as well as a list of their respective labels to test why the
    full RPE dataset seems to imply a different barrier than the shooting points.

--- NucleationModel/data_read.py
+++ NucleationModel/data_read.py
@@ -199,3 +199,17 @@ def read_RPE_and_TPS(
 		np.append(RPE_labels, TPS_labels, axis = 0), \
 		weights,\
 		np.append(RPE_names, TPS_names, axis = 0)
+
+def read_shooting_points(filename):
+	print("Read shooting point file")
+	with open(filename, "r") as file:
+		file.readline()
+		shooting_points = file.readlines()
+		precision = 2
+		shooting_points = np.array(\
+			[list(map(lambda x: round(float(x),precision),
+				point[:-1].split(" ")[1:])) for point in shooting_points])
+		labels = np.array([point[0] for point in shooting_points])
+		shooting_points = np.array([point[1:] for point in shooting_points])
+		print("{} shooting points read".format(len(labels)))
+		return shooting_points, labels
