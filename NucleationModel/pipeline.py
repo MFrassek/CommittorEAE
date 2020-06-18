de8c886b019c45614620d829fe468e7a6529d77b
Martin <martinfrassek@gmx.de>
2020-06-18 19:35:06 +0200

    Add functions to provide data for other class initializations
    Move the code to initizalise PlotData, StepwiseData and ImportanceData to
    this class.
    Duplicate process() and rename and modify the functions to rbn() [reduce,
    bound, normalize] and rbng() [reduce, bound, normalize, gridify].
    These two functions can provide all processing needed for the plot_data(),
    importance_data() and stepwise_data() functions.

--- NucleationModel/pipeline.py
+++ NucleationModel/pipeline.py
@@ -3,10 +3,13 @@ from bounder import Bounder
 from normalizer import Normalizer
 from gridifier import Gridifier
 
+import numpy as np
+
 class Pipeline():
 	def __init__(self, const, reduced_list_var_names, base_snapshots):
 		self._const = const
 		self._reduced_list_var_names = reduced_list_var_names
+		self._dimensions = len(reduced_list_var_names)
 		self._reducer = Reducer(
 			self._reduced_list_var_names, 
 			self._const.name_to_list_position)
@@ -18,10 +21,38 @@ class Pipeline():
 		self._gridifier = Gridifier(base_snapshots, self._const.resolution)
 		#base_snapshots = self._gridifier.gridify_snapshots(base_snapshots)
 
-	def process(self, snapshots):
+	def rbn(self, snapshots):
 		snapshots = self._reducer.reduce_snapshots(snapshots)
 		snapshots = self._bounder.bound_snapshots(snapshots)
 		snapshots = self._normalizer.normalize_snapshots(snapshots)
-		snapshots = self._gridifier.gridify_snapshots(snapshots)
-
 		return snapshots
+
+	def rbng(self, snapshots):
+		snapshots = self._reducer.reduce_snapshots(snapshots)
+		snapshots = self._bounder.bound_snapshots(snapshots)
+		snapshots = self._normalizer.normalize_snapshots(snapshots)
+		snapshots = self._gridifier.gridify_snapshots(snapshots)		
+		return snapshots
+
+	def plot_data(self, dataset):
+		return self.rbng(dataset.train_past_snapshots), \
+			np.zeros(self._dimensions), \
+			np.ones(self._dimensions)*(self._const.resolution - 1), \
+			dataset.train_labels, \
+			dataset.train_weights
+
+	def importance_data(self, dataset): 
+		return self.rbn(dataset.val_past_snapshots), \
+			self.rbn(dataset.val_snapshots), \
+			dataset.val_labels, \
+			dataset.val_weights
+
+	def stepwise_data(self, dataset):
+		return self.rbn(dataset.train_past_snapshots), \
+			self.rbn(dataset.train_snapshots), \
+			dataset.train_labels, \
+			dataset.train_weights, \
+			self.rbn(dataset.val_past_snapshots), \
+			self.rbn(dataset.val_snapshots), \
+			dataset.val_labels, \
+			dataset.val_weights
