de6f3f650f860c319f9ca519cfad0f73ce527784
Martin <martinfrassek@gmx.de>
2020-06-18 16:39:21 +0200

    Extracted bounder class from datasetData
    To make the bounding process possible for the shooting points
    and have a clearer separation of classes, the functions needed for bounding
    of snapshots are moved to their own class.

---
+++ NucleationModel/bounder.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class Bounder():
+	def __init__(self, snapshots, outlier_cutoff):
+		self._snapshots = snapshots
+		self._dimensions = len(snapshots[0])
+		self._outlier_cutoff = outlier_cutoff
+		self._lower_bound = np.percentile(
+			self._snapshots, 
+			100 * self._outlier_cutoff, 
+			axis = 0)
+		self._upper_bound = np.percentile(
+			self._snapshots, 
+			100 * (1 - self._outlier_cutoff),
+			axis = 0)
+
+	@property
+	def lower_bound(self):
+		return self._lower_bound
+
+	@property
+	def upper_bound(self):
+		return self._upper_bound
+			
+	def bound_snapshots(self, snapshots):
+		"""Set the values of a snapshopt that lie outside of the bounds to that 
+		bound while leaving the other values unchanged.
+		Initially transpose the snapshots to a column list
+		For each column, iterate over all entries and compare them to the 
+		lower or upper bound of that column. If they are lower or higher, 
+		change to the value of that bound.
+		Return the transpose of the column list, thereby yielding 
+		the cleaned snapshots.
+		"""
+		column_list = np.transpose(snapshots)
+		column_list = [[min(self.upper_bound[col_nr],\
+						max(self.lower_bound[col_nr],entry)) \
+						for entry in column_list[col_nr]] \
+						for col_nr in range(self._dimensions)]
+		return np.transpose(column_list)
