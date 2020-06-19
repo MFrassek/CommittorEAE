6e2361ec480ffbf86eb18197920630563cca43fe
Martin <martinfrassek@gmx.de>
2020-06-19 14:30:56 +0200

    Added three new processing functions rbnga(), rbngat() and rbngatb()
    rbnga() acts as rbng() but additionally  approximates the pBs based
    on the processed snapshots as well as given labels and weights.
    
    rbngat() acts as rbnga() but additionally uses the approximated pBs to
    trim the snapshots, labels, weights, pB_dict and pBs.
    
    rbngatb() acts as rbgnat() but additionally generates balanced weights for
    the pBs.

--- NucleationModel/pipeline.py
+++ NucleationModel/pipeline.py
@@ -2,6 +2,9 @@ from reducer import Reducer
 from bounder import Bounder
 from normalizer import Normalizer
 from gridifier import Gridifier
+from pB_approximator import pB_Approximator
+from trimmer import Trimmer
+from pB_balancer import pB_Balancer
 
 import numpy as np
 
@@ -36,17 +39,77 @@ class Pipeline():
 		return self._gridifier.maxima
 
 	def rbn(self, snapshots):
+		"""Reduce, bound and normalize snapshots."""
 		snapshots = self._reducer.reduce_snapshots(snapshots)
 		snapshots = self._bounder.bound_snapshots(snapshots)
 		snapshots = self._normalizer.normalize_snapshots(snapshots)
 		return snapshots
 
 	def rbng(self, snapshots):
+		"""Reduce, bound, normalize and gridify snapshots."""
 		snapshots = self._reducer.reduce_snapshots(snapshots)
 		snapshots = self._bounder.bound_snapshots(snapshots)
 		snapshots = self._normalizer.normalize_snapshots(snapshots)
 		snapshots = self._gridifier.gridify_snapshots(snapshots)		
 		return snapshots
+
+	def rbnga(self, snapshots, labels, weights):
+		"""Reduce, bound, normalize and gridify snapshots 
+		and approximate the pBs.
+		"""
+		snapshots = self._reducer.reduce_snapshots(snapshots)
+		snapshots = self._bounder.bound_snapshots(snapshots)
+		snapshots = self._normalizer.normalize_snapshots(snapshots)
+		snapshots = self._gridifier.gridify_snapshots(snapshots)		
+		pB_dict, pBs = pB_Approximator.approximate_pBs(
+			snapshots,
+			labels,
+			weights)
+		return snapshots, pB_dict, pBs
+
+	def rbngat(self, snapshots, labels, weights):
+		"""Reduce, bound, normalize and gridify snapshots,
+		approximate the pBs
+		and trimm the snapshots, labels and weights.
+		"""
+		snapshots = self._reducer.reduce_snapshots(snapshots)
+		snapshots = self._bounder.bound_snapshots(snapshots)
+		snapshots = self._normalizer.normalize_snapshots(snapshots)
+		snapshots = self._gridifier.gridify_snapshots(snapshots)		
+		pB_dict, pBs = pB_Approximator.approximate_pBs(
+			snapshots,
+			labels,
+			weights)
+		trimmer = Trimmer(pBs)
+		snapshots = trimmer.trim_snapshots(snapshots)
+		label = trimmer.trim_snapshots(labels)
+		weights = trimmer.trim_snapshots(weights)
+		pB_dict = trimmer.trim_dict(pB_dict)
+		pBs = trimmer.trim_snapshots(pBs)
+		return snapshots, labels, weights, pB_dict, pBs
+
+	def rbngatb(self, snapshots, labels, weights):
+		"""Reduce, bound, normalize and gridify snapshots,
+		approximate the pBs,
+		trimm the snapshots, labels and weights
+		and generate balanced weights for the pBs.
+		"""
+		snapshots = self._reducer.reduce_snapshots(snapshots)
+		snapshots = self._bounder.bound_snapshots(snapshots)
+		snapshots = self._normalizer.normalize_snapshots(snapshots)
+		snapshots = self._gridifier.gridify_snapshots(snapshots)		
+		pB_dict, pBs = pB_Approximator.approximate_pBs(
+			snapshots,
+			labels,
+			weights)
+		trimmer = Trimmer(pBs)
+		snapshots = trimmer.trim_snapshots(snapshots)
+		label = trimmer.trim_snapshots(labels)
+		weights = trimmer.trim_snapshots(weights)
+		pB_dict = trimmer.trim_dict(pB_dict)
+		pBs = trimmer.trim_snapshots(pBs)
+		pB_weights = pB_Balancer.balance(pBs, self._const.balance_bins)
+		return snapshots, labels, weights, pB_dict, pBs, pB_weights
 
 	def plot_data(self, dataset):
 		return self.rbng(dataset.train_past_snapshots), \
