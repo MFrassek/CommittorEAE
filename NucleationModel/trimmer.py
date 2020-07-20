bbda3527a46b81aba850ca65a4166d6a791343a6
Martin <martinfrassek@gmx.de>
2020-07-20 14:25:44 +0200

    Add HalfTrimmer to trimmer.py, to test trimming away all instances with pB > 0.5

--- NucleationModel/trimmer.py
+++ NucleationModel/trimmer.py
@@ -12,3 +12,16 @@ class Trimmer():
     def trim_dict(dict_to_trim):
         return {key: label for key, label in dict_to_trim.items()
                 if label != 0 and label != 1}
+
+
+class HalfTrimmer():
+    def __init__(self, pBs):
+        self._keep_elements = [ele != 0 and ele <= 0.5 for ele in pBs]
+
+    def trim_snapshots(self, list_to_trim):
+        return np.array(list_to_trim)[self._keep_elements]
+
+    @staticmethod
+    def trim_dict(dict_to_trim):
+        return {key: label for key, label in dict_to_trim.items()
+                if label != 0 and label <= 0.5}
