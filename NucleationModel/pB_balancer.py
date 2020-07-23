dd29439d8e2eb2ffaadd75dc13e3406df5ba05da
Martin <martinfrassek@gmx.de>
2020-07-23 14:12:19 +0200

    Fix total weight in pB_Balancer to a constant number
    As, so far no weight normalization according to the size of counter took place,
    the sum of all weights varied depending on the chosen bin size and the size
    of the counter. Like in the Hybercube_Balancer weights are now also divided
    by the number of non-empty bins. Thereby it is guaranteed that the total sum
    of all weight always equals to the number of snapshots.

--- NucleationModel/pB_balancer.py
+++ NucleationModel/pB_balancer.py
@@ -8,8 +8,9 @@ class pB_Balancer():
         pBs_len = len(pBs)
         round_pBs = np.ceil((np.array(pBs) * (bins + 1)))
         counter = Counter(round_pBs)
-        balanced_counter = {key: pBs_len/label for key, label
-                            in counter.items()}
+        counter_len = len(counter)
+        balanced_counter = {key: pBs_len / (label * counter_len)
+                            for key, label in counter.items()}
         pB_balanced_weights = np.array(
             [balanced_counter[i] for i in round_pBs])
         return pB_balanced_weights
@@ -19,10 +20,10 @@ class pB_Balancer():
         pBs_len = len(pBs)
         round_pBs = np.ceil((np.array(pBs) * (bins + 1)))
         counter = Counter(round_pBs)
-        masked_balanced_counter = {key: pBs_len/label
+        counter_len = len(counter)
+        masked_balanced_counter = {key: pBs_len / (label * counter_len)
                                    if key != 0 and key != bins + 1 else 0
-                                   for key, label
-                                   in counter.items()}
+                                   for key, label in counter.items()}
         masked_pB_balanced_weights = np.array(
             [masked_balanced_counter[i] for i in round_pBs])
         return masked_pB_balanced_weights
@@ -33,9 +34,9 @@ class pB_Balancer():
         trimmed_pBs_len = len(trimmed_pBs)
         trimmed_round_pBs = np.ceil((np.array(trimmed_pBs) * (bins + 1)))
         counter = Counter(trimmed_round_pBs)
-        trimmed_balanced_counter = {key: trimmed_pBs_len/label
-                                    for key, label
-                                    in counter.items()}
+        counter_len = len(counter)
+        trimmed_balanced_counter = {key: trimmed_pBs_len / (label * counter_len)
+                                    for key, label in counter.items()}
         trimmed_pB_balanced_weights = np.array(
             [trimmed_balanced_counter[i] for i in trimmed_round_pBs])
         return trimmed_pB_balanced_weights
