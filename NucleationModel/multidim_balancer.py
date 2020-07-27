f72c1a6c3b00ba3f9cac4a943e14b359317d274c
Martin <martinfrassek@gmx.de>
2020-07-27 15:55:22 +0200

    Write MultiDim_Balancer class to balance snapshots according to their distibution along all dimensions
    The Hypercube_Balancer did not fully fix the problem of bad reconstruction
    for high values. While it ensures that each hypercube has the same total
    weight, per dimensions there can still be a non-uniform distribution of
    hypercubes along the value range.
    In a new approach, each dimension will be balanced separatedly to constitute
    the total multi-dimensionally balanced weight.
    A similar balancing procedure as in pB_Balancer and Hypercube_Balancer is
    used, however here however once per dimension.
    The final list of weights is the normalized product of weights for each
    dimension.

---
+++ NucleationModel/multidim_balancer.py
@@ -0,0 +1,29 @@
+import numpy as np
+from collections import Counter
+
+
+class MultiDim_Balancer():
+    @staticmethod
+    def balance(snapshots, bins):
+        snapshot_len = len(snapshots)
+        minima = np.amin(snapshots, axis=0)
+        maxima = np.amax(snapshots, axis=0)
+        inverse_spans_x_resolution = \
+            1 / (maxima - minima) * (bins - 1)
+        round_snapshots = np.floor(
+            (snapshots - minima) * inverse_spans_x_resolution + 0.5)
+        hc_balanced_weights = np.ones(snapshot_len)
+        round_columns = np.transpose(round_snapshots)
+        for column in round_columns:
+            counter = Counter(column)
+            counter_len = len(counter)
+            # Balance weights such that all weight together sum to
+            # snapshot_len and the weights for each key sum together to
+            # snapshot_len / counter_len
+            balanced_counter = {key: snapshot_len / (label * counter_len)
+                                for key, label in counter.items()}
+            col_balanced_weights = np.array(
+                [balanced_counter[i] for i in column])
+            hc_balanced_weights *= col_balanced_weights
+        hc_balanced_weights /= np.mean(hc_balanced_weights)
+        return hc_balanced_weights
