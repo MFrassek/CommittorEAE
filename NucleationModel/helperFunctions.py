5e65f5471b1b3ada953b48a76577fd674e7d15a6
Martin <martinfrassek@gmx.de>
2020-08-06 17:57:44 +0200

    Write helperfunctions to, for each value along 1 or 2 dimensions calculate the mean value of the other dimensions
    So far, for plotting predictions along one (scatter) or two (map) dimensions
    all other dimensional values have been replaced with 0, as that is the mean
    of each dimension after the normalization.
    However several dimensions are highly correlated, meaning, that using the
    mean value for one dimension, while a highly correlated dimension has a high
    or low value, will force the model to extrapolat, making the predictions
    worse. Further the points will be unrealistic, and therefore not adding
    insight.
    To correct this, functions were written that calculate the mean along each
    dimension, given all distinct, gridified values along one or two dimensions.
    This way more representative values can be chosen, less extrapolation will
    occur and more realistic datapoints are going to be used.

--- NucleationModel/helperFunctions.py
+++ NucleationModel/helperFunctions.py
@@ -94,3 +94,37 @@ def print_coverage(list_var_names, dataset):
     plt.show()
     for i, _ in enumerate(AACoverageOfABRange):
         print("{}: {}".format(list_var_names[i], AACoverageOfABRange[i]))
+
+
+def get_means_from_tuples(tuples):
+    return np.mean(list(zip(*tuples)), axis=1)
+
+
+def get_1D_correlated_mean(grid_snapshots, x_int):
+    snapshots_per_x = {}
+    for snapshot in grid_snapshots:
+        # Append to key associated with this snapshots value for x
+        try:
+            snapshots_per_x[snapshot[x_int]].append(tuple(snapshot))
+        except Exception:
+            snapshots_per_x[snapshot[x_int]] = [tuple(snapshot)]
+    means_per_x = {}
+    for x in snapshots_per_x:
+        means_per_x[x] = get_means_from_tuples(snapshots_per_x[x])
+    return means_per_x
+
+
+def get_2D_correlated_mean(grid_snapshots, x_int, y_int):
+    snapshots_per_xy = {}
+    for snapshot in grid_snapshots:
+        # Append to key associated with this snapshots values for x and y
+        try:
+            snapshots_per_xy[(snapshot[x_int], snapshot[y_int])] \
+                .append(tuple(snapshot))
+        except Exception:
+            snapshots_per_xy[(snapshot[x_int], snapshot[y_int])] \
+                = [tuple(snapshot)]
+    means_per_xy = {}
+    for xy in snapshots_per_xy:
+        means_per_xy[xy] = get_means_from_tuples(snapshots_per_xy[xy])
+    return means_per_xy
