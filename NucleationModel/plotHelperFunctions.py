f39025fa17eec09701454e9df4b2cbca68990cf6
Martin <martinfrassek@gmx.de>
2020-08-06 11:32:39 +0200

    Revise plotting of encoder and decoder and move to plotHelperFunctions

--- NucleationModel/plotHelperFunctions.py
+++ NucleationModel/plotHelperFunctions.py
@@ -2,6 +2,9 @@ from plotter import Plotter
 from autoEncoder import AutoEncoder
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+from helperFunctions import function_to_str
 
 
 def plot_loss_history(history, file_name):
@@ -94,3 +97,105 @@ def plot_with_different_settings(
         maxima=maxima,
         fill_val=0,
         max_row_len=6)
+
+
+def plot_encoder_decoder(
+        const,
+        loss_function,
+        reduced_list_var_names,
+        reduced_name_to_list_position,
+        train_ds,
+        val_ds,
+        pipeline):
+    autoencoder, autoencoder_1, autoencoder_2, \
+        encoder, decoder_1, decoder_2 = \
+        AutoEncoder.model(len(reduced_list_var_names), loss_function, const)
+    history = autoencoder.fit(
+        x=train_ds,
+        epochs=const.epochs,
+        validation_data=val_ds,
+        callbacks=[tf.keras.callbacks.EarlyStopping(
+            monitor="val_loss", patience=3)])
+    plot_encoder(
+        reduced_list_var_names=reduced_list_var_names,
+        reduced_name_to_list_position=reduced_name_to_list_position,
+        pipeline=pipeline,
+        const=const,
+        loss_function=loss_function,
+        encoder=encoder)
+    plot_decoder(
+        const=const,
+        decoder_1=decoder_1)
+
+
+def plot_encoder(
+        reduced_list_var_names,
+        reduced_name_to_list_position,
+        pipeline,
+        const,
+        loss_function,
+        encoder):
+    Plotter.plot_super_map(
+        used_variable_names=reduced_list_var_names,
+        name_to_list_position=reduced_name_to_list_position,
+        lower_bound=pipeline.lower_bound,
+        upper_bound=pipeline.upper_bound,
+        const=const,
+        pre_stamp="EncoderTest"+"_{}".format(function_to_str(loss_function)),
+        method=Plotter.calc_map_generated,
+        model=encoder,
+        minima=pipeline.minima,
+        maxima=pipeline.maxima)
+
+
+def plot_decoder(
+        const, decoder_1):
+    x_int = 0
+    y_int = 1
+    minima = [-10, -10]
+    maxima = [10, 10]
+    fig, ax = plt.subplots(1, 1)
+    plt.imshow(
+        np.maximum(
+            np.transpose(
+                Plotter.calc_map_generated(
+                    x_pos=x_int,
+                    y_pos=y_int,
+                    minima=minima,
+                    maxima=maxima,
+                    resolution=const.resolution,
+                    model=decoder_1,
+                    fill_val=1)[0])[::-1],
+            const.logvmin / 2),
+        cmap=const.cmap,
+        interpolation='nearest',
+        norm=mpl.colors.LogNorm(
+            vmin=const.logvmin,
+            vmax=1.0-const.logvmin),
+        extent=[0, 1, 0, 1])
+    ax.set_xticks(np.linspace(0, 1, 3))
+    ax.set_xticklabels(
+        np.around(
+            np.linspace(
+                minima[x_int],
+                maxima[x_int],
+                3),
+            2),
+        rotation=60)
+    ax.set_yticks(np.linspace(0, 1, 3))
+    ax.set_yticklabels(np.around(
+        np.linspace(
+            minima[y_int],
+            maxima[y_int],
+            3),
+        2))
+    ax.set_xlabel(
+        "$bn{}$".format(x_int),
+        fontsize=const.subfig_size * 10)
+    ax.set_ylabel(
+        "$bn{}$".format(y_int),
+        fontsize=const.subfig_size * 10)
+    plt.colorbar(extend="both")
+    plt.tight_layout()
+    plt.savefig("results/Decoder_{}.png".format(const.model_stamp))
+    plt.show()
