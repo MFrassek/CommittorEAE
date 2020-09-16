f42824e9d7417c4b251ffd7852b5e7e90c6be103
Martin <martinfrassek@gmx.de>
2020-09-16 14:14:27 +0200

    Write functions to store model weights and load them into a new model later
    store_model_weights() and load_model_weights() were added to helperFunctions.py
    to store weights of a trained model and retrieve them at any later time point.
    Example calls to these functions were added NPA.ipynb.

--- NucleationModel/helperFunctions.py
+++ NucleationModel/helperFunctions.py
@@ -146,3 +146,27 @@ def make_components_normalized(formula_components):
 
 def flatten_list_of_lists(list_of_lists):
     return [y for x in list_of_lists for y in x]
+
+
+def store_model_weights(
+        path, autoencoder, autoencoder_1, autoencoder_2,
+        encoder, decoder_1, decoder_2):
+    autoencoder.save_weights("{}/autoencoder".format(path))
+    autoencoder_1.save_weights("{}/autoencoder_1".format(path))
+    autoencoder_2.save_weights("{}/autoencoder_2".format(path))
+    encoder.save_weights("{}/encoder".format(path))
+    decoder_1.save_weights("{}/decoder_1".format(path))
+    decoder_2.save_weights("{}/decoder_2".format(path))
+
+
+def load_model_weights(
+        path, autoencoder, autoencoder_1, autoencoder_2,
+        encoder, decoder_1, decoder_2):
+    autoencoder.load_weights("{}/autoencoder".format(path))
+    autoencoder_1.load_weights("{}/autoencoder_1".format(path))
+    autoencoder_2.load_weights("{}/autoencoder_2".format(path))
+    encoder.load_weights("{}/encoder".format(path))
+    decoder_1.load_weights("{}/decoder_1".format(path))
+    decoder_2.load_weights("{}/decoder_2".format(path))
+    return autoencoder, autoencoder_1, autoencoder_2, \
+        encoder, decoder_1, decoder_2
