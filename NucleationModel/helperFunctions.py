a9d2eb294a3208165bca5fb2229260eb6af94f95
Martin <martinfrassek@gmx.de>
2020-10-13 12:03:09 +0200

    Write helperfunction to drop unneeded dimensions from pickle files

--- NucleationModel/helperFunctions.py
+++ NucleationModel/helperFunctions.py
@@ -208,3 +208,12 @@ def merge_all_OPS_simulation_pickle_files(folder_name):
     pickle.dump(all_labels, open(
         "{}/all/labels.p".format(folder_name), "wb"))
     return all_paths, all_labels
+
+
+def discard_unwanted_dimensions_from_pickle_files(folder_name, keep_first_n):
+    paths = np.array(
+        pickle.load(open("{}/paths.p".format(folder_name), "rb")))
+    truncated_paths = [np.transpose(np.transpose(path)[:keep_first_n])
+                       for path in paths]
+    pickle.dump(
+        truncated_paths, open("{}/trunc_paths.p".format(folder_name), "wb"))
