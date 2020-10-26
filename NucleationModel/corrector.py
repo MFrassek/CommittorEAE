edc7feff32e1eeb74d8c3b545229a0d603d4cbb7
Martin <martinfrassek@gmx.de>
2020-10-26 11:17:01 +0100

    Extract attempt_adding_snapshot_to_dictionary_at_key() from correct_1D_point() and correct_2D_point()

--- NucleationModel/corrector.py
+++ NucleationModel/corrector.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 class Corrector():
     @classmethod
     def correct_1D_row(self, grid_snapshots):
         all_representations = {}
         for x in range(len(grid_snapshots[0])):
+            print(x)
             x_representation = self.correct_1D_point(
                 grid_snapshots=grid_snapshots,
                 x_int=x)
@@ -17,21 +19,27 @@ class Corrector():
     def correct_1D_point(self, grid_snapshots, x_int):
         snapshots_per_x = {}
         for snapshot in grid_snapshots:
-            # Append to key associated with this snapshots value for x
-            try:
-                snapshots_per_x[snapshot[x_int]].append(tuple(snapshot))
-            except Exception:
-                snapshots_per_x[snapshot[x_int]] = [tuple(snapshot)]
+            self.attempt_adding_snapshot_to_dictionary_at_key(
+                snapshots_per_x, snapshot, (snapshot[x_int]))
         means_per_x = {}
         for x in snapshots_per_x:
             means_per_x[x] = self.get_means_from_tuples(snapshots_per_x[x])
         return means_per_x
 
     @classmethod
+    def attempt_adding_snapshot_to_dictionary_at_key(
+            self, dictionary, snapshot, key):
+        try:
+            dictionary[key].append(tuple(snapshot))
+        except Exception:
+            dictionary[key] = [tuple(snapshot)]
+
+    @classmethod
     def correct_2D_grid(self, grid_snapshots):
         all_representations = {}
         for x in range(len(grid_snapshots[0])):
             for y in range(0, x):
+                print(x, y)
                 xy_representation = self.correct_2D_point(
                     grid_snapshots=grid_snapshots,
                     x_int=x,
@@ -46,13 +54,8 @@ class Corrector():
     def correct_2D_point(self, grid_snapshots, x_int, y_int):
         snapshots_per_xy = {}
         for snapshot in grid_snapshots:
-            # Append to key associated with this snapshots values for x and y
-            try:
-                snapshots_per_xy[(snapshot[x_int], snapshot[y_int])] \
-                    .append(tuple(snapshot))
-            except Exception:
-                snapshots_per_xy[(snapshot[x_int], snapshot[y_int])] \
-                    = [tuple(snapshot)]
+            self.attempt_adding_snapshot_to_dictionary_at_key(
+                snapshots_per_xy, snapshot, (snapshot[x_int], snapshot[y_int]))
         means_per_xy = {}
         for xy in snapshots_per_xy:
             means_per_xy[xy] = self.get_means_from_tuples(snapshots_per_xy[xy])
@@ -60,6 +63,3 @@ class Corrector():
 
     def get_means_from_tuples(tuples):
         return np.mean(list(zip(*tuples)), axis=1)
-
-    def get_modes_from_tuples(tuples):
-        return stats.mode(tuples)[0][0]
