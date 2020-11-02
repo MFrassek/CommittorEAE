c0a3d72e20365c7158800e0011fb07c6d992d8cf
Martin <martinfrassek@gmx.de>
2020-11-02 17:34:54 +0100

    Extract get_weighted_label_dict_and_weight_dict() from approximate_pBs()

--- NucleationModel/pB_approximator.py
+++ NucleationModel/pB_approximator.py
@@ -4,23 +4,30 @@ import numpy as np
 class pB_Approximator():
     @staticmethod
     def approximate_pBs(grid_snapshots, labels, weights):
-        weighted_label_dict = {}
-        weight_dict = {}
-        for snapshot, label, weight in zip(grid_snapshots, labels, weights):
-            gridpoint_tuple = tuple(snapshot)
-            if gridpoint_tuple in weighted_label_dict:
-                update_dictionary_entries(
-                    weighted_label_dict, weight_dict,
-                    gridpoint_tuple, weight, label)
-            else:
-                add_dictionary_entries(
-                    weighted_label_dict, weight_dict,
-                    gridpoint_tuple, weight, label)
+        weighted_label_dict, weight_dict = \
+            get_weighted_label_dict_and_weight_dict(
+                grid_snapshots, labels, weights)
         pB_dict = {key: weighted_label_dict[key] / weight_dict[key]
                    for key in weight_dict}
         pBs = [pB_dict[tuple(key)] for key in grid_snapshots]
         pB_weights = [weight_dict[tuple(key)] for key in grid_snapshots]
         return pB_dict, np.array(pBs), pB_weights
+
+
+def get_weighted_label_dict_and_weight_dict(grid_snapshots, labels, weights):
+    weighted_label_dict = {}
+    weight_dict = {}
+    for snapshot, label, weight in zip(grid_snapshots, labels, weights):
+        gridpoint_tuple = tuple(snapshot)
+        if gridpoint_tuple in weighted_label_dict:
+            update_dictionary_entries(
+                weighted_label_dict, weight_dict,
+                gridpoint_tuple, weight, label)
+        else:
+            add_dictionary_entries(
+                weighted_label_dict, weight_dict,
+                gridpoint_tuple, weight, label)
+    return weighted_label_dict, weight_dict
 
 
 def update_dictionary_entries(
