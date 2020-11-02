43cce8dd8753d4fb6fae9ea4b90f3508783bcfd0
Martin <martinfrassek@gmx.de>
2020-11-02 17:39:30 +0100

    Extract get_pB_dict(), get_pBs_list(), and get_pB_weight_list() from approximate_pBs()

--- NucleationModel/pB_approximator.py
+++ NucleationModel/pB_approximator.py
@@ -7,11 +7,10 @@ class pB_Approximator():
         weighted_label_dict, weight_dict = \
             get_weighted_label_dict_and_weight_dict(
                 grid_snapshots, labels, weights)
-        pB_dict = {key: weighted_label_dict[key] / weight_dict[key]
-                   for key in weight_dict}
-        pBs = [pB_dict[tuple(key)] for key in grid_snapshots]
-        pB_weights = [weight_dict[tuple(key)] for key in grid_snapshots]
-        return pB_dict, np.array(pBs), pB_weights
+        pB_dict = get_pB_dict(weighted_label_dict, weight_dict)
+        pBs = get_pBs_list(pB_dict, grid_snapshots)
+        pB_weights = get_pB_weight_list(weight_dict, grid_snapshots)
+        return pB_dict, pBs, pB_weights
 
 
 def get_weighted_label_dict_and_weight_dict(grid_snapshots, labels, weights):
@@ -40,3 +39,16 @@ def add_dictionary_entries(
         weighted_label_dict, weight_dict, gridpoint_tuple, weight, label):
     weighted_label_dict[gridpoint_tuple] = weight * label
     weight_dict[gridpoint_tuple] = weight
+
+
+def get_pB_dict(weighted_label_dict, weight_dict):
+    return {key: weighted_label_dict[key] / weight_dict[key]
+            for key in weight_dict}
+
+
+def get_pBs_list(pB_dict, grid_snapshots):
+    return np.array([pB_dict[tuple(key)] for key in grid_snapshots])
+
+
+def get_pB_weight_list(weight_dict, grid_snapshots):
+    return [weight_dict[tuple(key)] for key in grid_snapshots]
