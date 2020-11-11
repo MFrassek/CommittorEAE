f4f33a10e8ba45fbd2d251a085eccb3ecd209531
Martin <martinfrassek@gmx.de>
2020-11-11 18:11:48 +0100

    Add stept to gridify_snapshots to convert array into array of ints

--- NucleationModel/gridifier.py
+++ NucleationModel/gridifier.py
@@ -26,9 +26,11 @@ class Gridifier():
         """Take a list of snapshots and bin all entries to the closest
         gridpoint.
         """
-        return self.broadcast_round_to_closest_integer(
-            self.broadcast_rescale_to_resolution_range(
-                self.broadcast_shift_values_to_start_at_minimum(snapshots)))
+        return self.convert_array_to_int_array(
+            self.broadcast_round_to_closest_integer(
+                self.broadcast_rescale_to_resolution_range(
+                    self.broadcast_shift_values_to_start_at_minimum(
+                        snapshots))))
 
     def broadcast_shift_values_to_start_at_minimum(self, snapshots):
         return snapshots - self.minima
@@ -38,3 +40,6 @@ class Gridifier():
 
     def broadcast_round_to_closest_integer(self, snapshots):
         return np.floor(snapshots + 0.5)
+
+    def convert_array_to_int_array(self, snapshots):
+        return snapshots.astype(int)
