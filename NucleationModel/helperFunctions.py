ece222c3347a49c27bd7052799f151f926b26099
Martin <martinfrassek@gmx.de>
2020-12-08 15:57:41 +0100

    Remove dead code: discard_unwanted_dimensions_from_pickle_files()

--- NucleationModel/helperFunctions.py
+++ NucleationModel/helperFunctions.py
@@ -2,12 +2,3 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 from os import listdir
-
-
-def discard_unwanted_dimensions_from_pickle_files(folder_name, keep_first_n):
-    paths = np.array(
-        pickle.load(open("{}/paths.p".format(folder_name), "rb")))
-    truncated_paths = [np.transpose(np.transpose(path)[:keep_first_n])
-                       for path in paths]
-    pickle.dump(
-        truncated_paths, open("{}/trunc_paths.p".format(folder_name), "wb"))
