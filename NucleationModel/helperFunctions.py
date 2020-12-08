e020b1458283008f5014e829d4567d13c2eb5466
Martin <martinfrassek@gmx.de>
2020-12-08 15:56:34 +0100

    Remove dead code: merge_all_OPS_simulation_pickle_files()

--- NucleationModel/helperFunctions.py
+++ NucleationModel/helperFunctions.py
@@ -4,28 +4,6 @@ import pickle
 from os import listdir
 
 
-def merge_all_OPS_simulation_pickle_files(folder_name):
-    file_names = listdir(folder_name)
-    all_paths = []
-    all_labels = []
-    for file_name in file_names:
-        if file_name.endswith("paths.p"):
-            paths = np.array(
-                pickle.load(open("{}/{}".format(
-                    folder_name, file_name), "rb")))
-            all_paths = np.append(all_paths, paths)
-        if file_name.endswith("labels.p"):
-            labels = np.array(
-                pickle.load(open("{}/{}".format(
-                    folder_name, file_name), "rb")))
-            all_labels = np.append(all_labels, labels)
-    pickle.dump(all_paths, open(
-        "{}/all/paths.p".format(folder_name), "wb"))
-    pickle.dump(all_labels, open(
-        "{}/all/labels.p".format(folder_name), "wb"))
-    return all_paths, all_labels
-
-
 def discard_unwanted_dimensions_from_pickle_files(folder_name, keep_first_n):
     paths = np.array(
         pickle.load(open("{}/paths.p".format(folder_name), "rb")))
